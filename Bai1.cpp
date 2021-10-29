3c91a88843eb245417be9a3a391740469e1c679a
lqt1893 <93394403+lqt1893@users.noreply.github.com>
2021-10-29 22:45:27 +0700

    Moi sua

--- Bai1.cpp
+++ Bai1.cpp
@@ -5,7 +5,18 @@ int main()
 	printf("Nhap so nguyen: ");
 	scanf("%d", &a);
 	if (a>=1&&a<=9)
-		printf("%d", a);
+		switch (a)
+		{
+			case 1:printf("mot"); break;
+			case 2:printf("hai"); break;
+			case 3:printf("ba"); break;
+			case 4:printf("bon"); break;
+			case 5:printf("nam"); break;
+			case 6:printf("sau"); break;
+			case 7:printf("bay"); break;
+			case 8:printf("tam"); break;
+			case 9:printf("chin"); break;
+		}
 	else
 		printf("Khong doc duoc");
 }
